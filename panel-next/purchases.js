3c5b2e42aa6ab8e6f2875a25e75dbef9891b24e4
cevicheselchava <aguachileselchava@gmail.com>
2026-09-10 12:00:57 -0600

    Carga configuración de tostadas en Compras

--- panel-next/purchases.js
+++ panel-next/purchases.js
@@ -47,7 +47,8 @@ document.addEventListener('click',event=>{
   openPurchases();
 },true);
 
-import('./purchase-system-final.js?v=20260910-0617')
+import('./tostadas-config.js?v=20260910-1300')
+  .then(()=>import('./purchase-system-final.js?v=20260910-0617'))
   .then(()=>import('./purchase-stock-reference.js?v=20260910-0630'))
   .then(()=>import('./purchase-dish-buttons.js?v=20260910-0828'))
   .then(()=>{
